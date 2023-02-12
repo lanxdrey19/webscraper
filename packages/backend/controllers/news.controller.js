6fc3f24c2ccf21712dc95030e1d195356bbefc70
Lance Delos Reyes <lan_drey19@hotmail.com>
2023-02-13 00:29:14 +1300

    :alien: rewrite news api from new news source

--- packages/backend/controllers/news.controller.js
+++ packages/backend/controllers/news.controller.js
@@ -7,81 +7,24 @@ const httpStatus = require("http-status");
 const getNews = async (req, res) => {
   try {
     const arrayNews = [];
-    await axios(process.env.NEWS_BASE + process.env.NEWS_CATEGORY)
+    await axios(process.env.NEWS_SOURCE)
       .then((response) => {
         const html = response.data;
         const websiteData = cheerio.load(html);
 
-        websiteData(".thumbnail-wrapper", html).each(function () {
-          const info = websiteData(this).find("a").attr("title");
+        websiteData(".y8HYJ-y_lTUHkQIc1mdCq", html).each(function () {
+          const info = websiteData(this)
+            .find("a")
+            .find("div")
+            .find("h3")
+            .text();
           const link =
             process.env.NEWS_BASE + websiteData(this).find("a").attr("href");
-          const imgSrc = websiteData(this).find("span").find("img").attr("src");
-          arrayNews.push({ info, link, imgSrc });
+
+          arrayNews.push({ info, link });
         });
       })
       .catch((err) => console.log(err));
-
-    // let colNum = 0;
-    // let day = 0;
-    // let track = "";
-    // let artist = "";
-    // let finalArray = [];
-    // let thisMonthOnly = true;
-    // let imgSrc = "";
-
-    // for (let i = 0; i < arrayComebacks.length; i++) {
-    //   if (thisMonthOnly) {
-    //     if (colNum === 0) {
-    //       const subStrInfo = arrayComebacks[i].info.substring(
-    //         0,
-    //         arrayComebacks[i].info.length - 1
-    //       );
-    //       if (Number.isInteger(parseInt(subStrInfo))) {
-    //         if (parseInt(subStrInfo) >= day) {
-    //           day = parseInt(subStrInfo);
-    //           colNum = 1;
-    //         } else {
-    //           thisMonthOnly = false;
-    //         }
-    //       } else if (subStrInfo !== "TBA") {
-    //         const subStrInfo = arrayComebacks[i].info.substring(
-    //           0,
-    //           arrayComebacks[i].info.length - 1
-    //         );
-    //         track = subStrInfo;
-    //         colNum = 2;
-    //       }
-    //     } else if (colNum === 1) {
-    //       const subStrInfo = arrayComebacks[i].info.substring(
-    //         0,
-    //         arrayComebacks[i].info.length - 1
-    //       );
-    //       track = subStrInfo;
-    //       colNum = 2;
-    //     } else if (colNum === 2) {
-    //       const subStrInfo = arrayComebacks[i].info.substring(
-    //         0,
-    //         arrayComebacks[i].info.length - 1
-    //       );
-    //       artist = subStrInfo;
-
-    //       const link = arrayComebacks[i].link;
-    //       await axios(process.env.FANDOM_BASE_URL + link).then((res) => {
-    //         const htmlProfile = res.data;
-    //         const secondPageData = cheerio.load(htmlProfile);
-    //         secondPageData(".pi-image-thumbnail", htmlProfile).each(
-    //           function () {
-    //             imgSrc = secondPageData(this).attr("src");
-    //           }
-    //         );
-    //       });
-
-    //       colNum = 0;
-    //       finalArray.push({ day, track, artist, imgSrc });
-    //     }
-    //   }
-    // }
 
     res.status(httpStatus.OK).json(arrayNews);
   } catch (err) {
