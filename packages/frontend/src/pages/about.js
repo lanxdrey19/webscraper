4fbdbac5072982489642a1e48f41386f3ef304d3
Lance Delos Reyes <lan_drey19@hotmail.com>
2023-02-14 01:32:59 +1300

    :sparkles: add get static props

--- packages/frontend/src/pages/about.js
+++ packages/frontend/src/pages/about.js
@@ -1,13 +1,33 @@
 import Link from "next/link";
 
-function About() {
+function About({ users }) {
   return (
     <div>
       <h1>About</h1>
       <Link href="/event">Our Events</Link>
       <Link href="/event/1/photo/1">Our Special Event</Link>
+      <h1>Committee</h1>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
 
 export default About;
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+
+  return {
+    props: {
+      users: data,
+    },
+  };
+}
