1ab06a50ebde24190b1baefdf87ce1da2a5ffb57
Lance Delos Reyes <lan_drey19@hotmail.com>
2023-02-19 04:30:25 +1300

    :sparkles: add dynamic props using placeholder api

--- packages/frontend/src/pages/event/[eventId]/index.js
+++ packages/frontend/src/pages/event/[eventId]/index.js
@@ -1,13 +1,53 @@
 import { useRouter } from "next/router";
 
-function EventDetail() {
+function EventDetail({ post }) {
   const router = useRouter();
   const eventId = router.query.eventId;
   return (
     <>
-      <h1>Details about event {eventId}</h1>
+      <h1>
+        {post.id} {post.title}
+      </h1>
+      <p>{post.body}</p>
     </>
   );
 }
 
 export default EventDetail;
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+          eventId: "1",
+        },
+      },
+      {
+        params: {
+          eventId: "2",
+        },
+      },
+      {
+        params: {
+          eventId: "3",
+        },
+      },
+    ],
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.eventId}`
+  );
+  const data = await response.json();
+
+  return {
+    props: {
+      post: data,
+    },
+  };
+}
--- packages/frontend/src/pages/event/index.js
+++ packages/frontend/src/pages/event/index.js
@@ -1,11 +1,34 @@
-function Event() {
+import Link from "next/link";
+
+function Event({ posts }) {
   return (
     <>
-      <h1>Event 1</h1>
-      <h1>Event 2</h1>
-      <h1>Event 3</h1>
+      <h1>Lists of Events</h1>
+      {posts.map((post) => {
+        return (
+          <div key={post.id}>
+            <Link href={`event/${post.id}`} passHref>
+              <h2>
+                {post.id} {post.title}
+              </h2>
+            </Link>
+            <hr />
+          </div>
+        );
+      })}
     </>
   );
 }
 
 export default Event;
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+
+  return {
+    props: {
+      posts: data.slice(0, 3),
+    },
+  };
+}
